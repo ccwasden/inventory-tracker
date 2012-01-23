6ac8dd64fad5ffae6dc8d0952bd5367166bc41b0
Jason Larsen <jason.larsen@gmail.com>
2012-01-22 23:06:43 -0700

    Added Item and Product

---
+++ src/model/Item.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.util.*; 
+
+/**
+ * A physical instance of a particular Product. An Item correpsronds to a physical
+ * container with a barcode on it. For example, a case of soda might contain 24
+ * cans of Diet Coke. In this case, the Product is, "Diet Coke, 12 fl oz," while
+ * each phsyical can is a distinct Item.
+ * 
+ */
+public class Item {
+    
+    /**
+     * Constructs a new Item
+     *
+     */
+    public Item(){
+    	
+    }
+}
---
+++ src/model/Product.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.util.*; 
+
+/**
+ * A bar-coded product that can be stored in a StorageUnit.
+ * 
+ */
+public class Product {
+    
+    /**
+     * Constructs a new Item
+     *
+     */
+    public Product() {
+    	
+    }
+
+
+}
