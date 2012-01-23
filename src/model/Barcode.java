bd6006c78e07b4b4c95b843f96a38f86077b4138
Jason Larsen <jason.larsen@gmail.com>
2012-01-23 16:41:09 -0700

    added barcode object

---
+++ src/model/Barcode.java
@@ -0,0 +1,32 @@
+package model;
+
+/**
+* Represents a Barcode.
+*/
+public class Barcode {
+	private String _id;
+
+	/**
+	* Constructs a new Barcode object.
+	* @param id The value of the Barcode
+	*/
+	public Barcode(String id) {
+		
+	}
+
+	/**
+	* Gets the Barcode's ID.
+	* @return The Barcode's number.
+	*/
+	public String getID() {
+		
+	}
+
+	/**
+	* Sets the Barcode's ID.
+	* @param id The new value for the Barcode.
+	*/
+	public setID(String id) {
+		
+	}
+}
