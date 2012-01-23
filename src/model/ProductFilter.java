870a9f931438bf869695ccc8ba5677a9339eced0
ccwasden <wazztone@gmail.com>
2012-01-23 16:46:06 -0700

    Update src/model/ProductFilter.java

--- src/model/ProductFilter.java
+++ src/model/ProductFilter.java
@@ -1,5 +1,13 @@
 package model;
-
+/**
+ * FIlter for products 
+ */
 public class ProductFilter {
-
+  
+    /**
+     * Checks if product applies to filter
+     * @param product (Product) The product to run filter on
+     * @return boolean true if product is in filter
+     */
+    public bool productInFilter(Product product){}
 }
