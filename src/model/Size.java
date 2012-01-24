d92dc4361c2a715634d0fc0cdee54db0345a523d
Jason Larsen <jason.larsen@gmail.com>
2012-01-23 22:25:24 -0700

    added javadocs to size

--- src/model/Size.java
+++ src/model/Size.java
@@ -2,27 +2,51 @@ package model;
 
 import gui.common.SizeUnits;
 
+/**
+* Represents the size of a product.
+*/
 class Size {
 	private float _size;
 	private SizeUnits _units;
 
+	/**
+	* Creates a new Size object.
+	* @param size The quantity of the size.
+	* @param units The unit of measurement.
+	*/
 	public Size(float size, SizeUnits units) {
 		_size = size;
 		_units = units;
 	}
 
+	/**
+	* Get the size.
+	* @return The size.
+	*/
 	public float getSize() {
 		return _size;
 	}
 
+	/**
+	* Get the units for this size.
+	* @return The unit as a SizeUnit.
+	*/
 	public SizeUnits getUnits() {
 		return _units;
 	}
 
+	/**
+	* Set the size
+	* @param size The new size.
+	*/
 	public void setSize(float size) {
 		_size = size;
 	}
 
+	/**
+	* Set the units.
+	* @param units the new units.
+	*/
 	public void setUnits(SizeUnits units) {
 		_units = units;
 	}
