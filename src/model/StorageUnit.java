6d94218c9e280e1be04d5bd157f16d805bb6bc7b
danj256 <danj256@gmail.com>
2012-02-01 20:58:53 -0700

    StorageUnit Test() additions

--- src/model/StorageUnit.java
+++ src/model/StorageUnit.java
@@ -1,5 +1,9 @@
 package model;
 
+import gui.common.SizeUnits;
+
+import java.sql.Timestamp;
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 import org.json.JSONArray;
@@ -97,7 +101,7 @@ public class StorageUnit extends ProductContainer {
 	 * @return true if this StorageUnit can be edited, otherwise false
 	 */
 	public boolean canEditStorageUnit(String name) {
-		if (name == "")
+		if (name == "" || name == null)
 			return false;
 		// Make sure to also have a check above this to make sure that there
 		// are no other StorageUnits with the same name
@@ -156,7 +160,49 @@ public class StorageUnit extends ProductContainer {
 	* Static method for unit testing purposes.
 	* @return true if success
 	*/
-	public static boolean Test(){
+	public static boolean Test() {
+		
+		StorageUnit storageUnit1 = new StorageUnit("storageUnit1");
+		Barcode barcode1 = new Barcode(123);
+		Size size1 = new Size((float)3.4, SizeUnits.Liters);
+		Product product1 = new Product(barcode1, "description of product 1",
+				(float)5.5, size1, 6);
+		assert (storageUnit1.getItems().size() == 0);
+		
+		Timestamp expirationDate1 = new Timestamp(55555);
+		Item item1 = new Item(barcode1, product1, storageUnit1, expirationDate1);
+		storageUnit1.addItem(item1);
+		assert (storageUnit1.getItems().size() == 1);
+		
+		Barcode barcode2 = new Barcode(234);
+		Timestamp expirationDate2 = new Timestamp(666666);
+		Item item2 = new Item(barcode2, product1, storageUnit1, expirationDate2);
+		storageUnit1.addItem(item2);
+		assert (storageUnit1.getItems().size() == 2);
+		
+		Barcode barcode3 = new Barcode(345);
+		Barcode barcode4 = new Barcode(456);
+		Item item3 = new Item(barcode3, product1, storageUnit1, expirationDate1);
+		Item item4 = new Item(barcode4, product1, storageUnit1, expirationDate1);
+		TreeSet<Item> items3and4 = new TreeSet<Item>();
+		items3and4.add(item3);
+		items3and4.add(item4);
+		storageUnit1.addItems(items3and4);
+		assert (storageUnit1.getItems().size() == 4);
+		
+		storageUnit1.removeItem(item3);
+		assert (storageUnit1.getItems().size() == 3);
+		assert (!storageUnit1.getItems().contains(item3));
+		assert (storageUnit1.getItems().contains(item4));
+		assert (!storageUnit1.canEditStorageUnit(""));
+		assert (!storageUnit1.canEditStorageUnit(null));
+		
+		assert (storageUnit1.getName() == "storageUnit1");
+		storageUnit1.editStorageUnit("storageUnitNewName", items3and4);
+		assert (storageUnit1.getName() == "storageUnitNewName");
+		assert (!storageUnit1.getItems().contains(item1));
+		assert (storageUnit1.getItems().contains(item4));	
+		
 		return true;
 	}
 
